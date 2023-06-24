1db5c3e4b21324aceb2b09c3ae7ba003a8278bdb
Khoi Trinh <116620830+KHOITRINH03@users.noreply.github.com>
2023-06-24 11:46:30 -0400

    Creat Array DAS

--- ARRAY/index.js
+++ ARRAY/index.js
@@ -1,1 +1,50 @@
+## All you MUST know about Array in Array Data Structure
 
+1. Array is a collection of items which have same value types.
+
+2. In Javascript, there are 2 types of Array containing Static Array (fixed) and Dynamic Array.
+
+3. Array store elements one after the order in contiguous memory.
+
+4. Array are recommended when you know exactly how many elements required beforehand.
+
+5. Array is a linear data structure.
+
+
+
+// ---------- Array Data Structure ---------
+
+class MyArray {
+  constructor() {
+    this.length = length;
+    this.data = {};
+  }
+
+  get(index) { // O(1)
+    
+  }
+
+  peek() { // O(1)
+
+  }
+
+  push() { // O(1)
+
+  }
+
+  pop() { // O(1)
+
+  }
+
+  delete(index) { // O(n)
+
+  }
+
+  shiftItems() {
+
+  }
+}
+
+const newArray = new MyArray();
+
+console.log(newArray);
