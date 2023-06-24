ac123448264dd7456dd7ee68a4c1cd072d10591f
Khoi Trinh Nhat <johntrinhnhat@gmail.com>
2023-06-24 14:07:07 -0700

    Update Hash Table

---
+++ HashTable/index.js
@@ -0,0 +1,31 @@
+/* ## All you MUST know about Hash-Table Data Structure:
+
+1. Hash table stores elements in key-value paris format.
+
+2. Hash Function is used to convert a range of keys into a range of 
+
+*/
+
+// ---------- Hash Table Data Structure ----------
+
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    //hash function
+    _hash(key) { 
+
+    }
+
+    set(key, value) {
+
+    }
+
+    get(key) {
+        
+    }
+}
+
+const hashTable = new HashTable();
+console.log(hashTable)
