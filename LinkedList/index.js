2c454aff48ccb0cbc4c057a2f7a1f2cdc4b63912
KHOITRINH03 <Johntrinhnhat@gmail.com>
2023-06-24 21:32:36 -0400

    Create basic linked list

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -1,1 +1,34 @@
+/* ## All you MUST know about LinkedList Data Structure:
 
+1. 
+
+2. 
+
+3. 
+
+4. 
+
+*/
+
+// ---------- Linked Lists Data Structure ----------
+
+class Node {
+  constructor() {
+    this.value = value;
+    this.pointer = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+const linkedList = new LinkedList(100);
+console.log(linkedList);
